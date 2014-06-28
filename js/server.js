c08a258fae56e40063a0b8a57dc3d78dfc2c2a7a
Alex Treppass <alextreppass@gmail.com>
2014-06-28 18:23:39 +0100

    server

---
+++ js/server.js
@@ -0,0 +1,14 @@
+var PeerServer = require('peer').PeerServer;
+var server = new PeerServer({
+  port: 9000,
+  path: '/tanks',
+  allow_discovery: true
+});
+
+server.on('connection', function(id) {
+  console.log('client connected! ' + id);
+});
+
+server.on('disconnect', function(id) {
+  console.log('client disconnected! ' + id);
+});
